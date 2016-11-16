ebbc86b245fa284dabc8bf443823f1b68cdf1b0e
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-15 19:24:33 -0800

    Make simple view and connect the outlets

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var arrayInString = [String]()
+    var arrayInInteger = [Int]()
+    
+    
+    @IBOutlet weak var resultLabel: UILabel! = UILabel()
+   
+   
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        
+        let number: Int? = Int((sender.titleLabel?.text!)!)
+        arrayInString.append(String(number!))
+        arrayInInteger.append(number!)
+        resultLabel.text = arrayInString.joined(separator: "")
+        print(Int(resultLabel.text!)! + 1)
+    }
+    
+    @IBAction func plusButton(_ sender: UIButton) {
+        if arrayInInteger.count == 0 {
+            print("nothing to be added!")
+        } else {
+            
+        }
+    }
+    
+    @IBAction func minusButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultLabel.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
